3fdcae67b3d8fccb66cf5bcd2fe88adb5564ddee
mohammadmehdikarimi <karimimohammadmehdi99@gmail.com>
2022-09-16 10:17:30 +0000

    learning lits

---
+++ python.py
@@ -0,0 +1,17 @@
+#lists 
+
+names = ['sarah', 'john', 'mark']
+
+names2 = ['wolf', 'lion']
+
+names.append('margot')
+
+print(names)
+
+print(names2)
+
+print(names + names2)
+
+names[1:2] = []
+
+print(names)
