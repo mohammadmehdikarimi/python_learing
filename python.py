7e4840b673514c1ed74975ee5a41dc9bcc997e45
mohammadmehdikarimi <karimimohammadmehdi99@gmail.com>
2022-09-20 09:52:30 +0000

    learning decorative property

--- python.py
+++ python.py
@@ -411,5 +411,29 @@ print(p._Person__height)  #name mangling
 
 # decorative property
 
+class Person:
+    def __init__(self, fname, lname) -> None:
+        self.fname = fname
+        self.lname = lname
+
+    def full_name(self):
+        return f'{self.fname} {self.lname}'
+
+    @property
+    def email(self):
+       return f'{self.fname}_{self.lname}@email.com'
+
+
+a = Person('john', 'doo')
+a.fname = 'mark'
+
+print(a.fname)
+print(a.lname)
+print(a.email)
+print(a.full_name())
+
+
+
+
 
 
